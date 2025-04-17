add790d3b898d90306cd06c143137c7951a2d076
Vivek Kadam <vk2567298@gmail.com>
2025-04-17 12:29:36 +0530

    signup login integration

---
+++ index.js
@@ -0,0 +1,34 @@
+import express from "express";
+import env from "dotenv";
+import dbConnect from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+env.config();
+
+
+const app=express();
+const port=process.env.PORT;
+app.use(express.json());
+app.use(cookieParser());
+
+const allowedOrigins = ["http://localhost:5173"];
+
+app.use(cors({
+    origin: allowedOrigins,
+    credentials: true,
+}));
+app.use('/api/auth', authRoutes);
+// app.use('/api/artGenerator',avatarRoute);
+
+app.get("/",(req,res)=>{
+    res.send("This is home page")
+})
+
+dbConnect().then(() => {
+    app.listen(port,()=>{
+        console.log(`server running on port ${port}`);
+    })
+}).catch(err => {
+    console.error("Database connection failed:", err);
+});
