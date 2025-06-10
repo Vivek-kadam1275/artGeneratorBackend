f1c34e984b8ad6dfeb484daa80a8a28e41389129
Vivek Kadam <vk2567298@gmail.com>
2025-06-11 00:21:20 +0530

    frontend link cors integration

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 
 app.use(cookieParser());
 
-const allowedOrigins = ["http://localhost:5173","http://localhost:5174"];
+const allowedOrigins = ["http://localhost:5173","http://localhost:5174","https://peppy-kitsune-b3ffbc.netlify.app"];
 
 app.use(cors({
     origin: allowedOrigins,
