fcc15a920e3e9b5c4d21d2d2c1a5dd59ad5df633
Vivek Kadam <vk2567298@gmail.com>
2025-06-11 10:29:10 +0530

    added test route for scribble

--- controllers/artController.js
+++ controllers/artController.js
@@ -143,3 +143,31 @@ export const generateScribble = async (req, res) => {
   }
 
 }
+
+
+export const testScribble= async (req, res) => {
+  const testPrompt = "a colorful turtle";
+  const testScribble =
+    "https://replicate.delivery/pbxt/IJE6zP4jtdwxe7SffC7te9DPHWHW99dMXED5AWamlBNcvxn0/user_1.png";
+
+    console.log(testPrompt);
+  try {
+    const output = await replicate.run(
+      "jagilley/controlnet-scribble:435061a1b5a4c1e26740464bf786efdfa9cb3a3ac488595a2de23e143fdb0117",
+      {
+        input: {
+          image: testScribble,
+          prompt: testPrompt,
+          image_resolution: "512",
+          scale: 7,
+        },
+      }
+    );
+
+    console.log("✅ Output from Replicate:", output[1].url());
+    return res.status(200).json({ result: output[1].url() });
+  } catch (err) {
+    console.error("❌ Replicate Error:", err);
+    res.status(500).json({ error: "Failed to generate" });
+  }
+}
--- routes/artRoutes.js
+++ routes/artRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { generateDalle, generateScribble, getImages, saveImage } from "../controllers/artController.js";
+import { generateDalle, generateScribble, getImages, saveImage, testScribble } from "../controllers/artController.js";
 import { auth } from "../middlewares/authMiddleware.js";
 const router = express.Router();
 
@@ -16,31 +16,6 @@ router.post("/scribble",generateScribble);
 
 
 
-// router.post("/test-scribble", async (req, res) => {
-//   const testPrompt = "a colorful turtle";
-//   const testScribble =
-//     "https://replicate.delivery/pbxt/IJE6zP4jtdwxe7SffC7te9DPHWHW99dMXED5AWamlBNcvxn0/user_1.png";
-
-//     console.log(testPrompt);
-//   try {
-//     const output = await replicate.run(
-//       "jagilley/controlnet-scribble:435061a1b5a4c1e26740464bf786efdfa9cb3a3ac488595a2de23e143fdb0117",
-//       {
-//         input: {
-//           image: testScribble,
-//           prompt: testPrompt,
-//           image_resolution: "512",
-//           scale: 7,
-//         },
-//       }
-//     );
-
-//     console.log("✅ Output from Replicate:", output[1].url());
-//     return res.status(200).json({ result: output[0] });
-//   } catch (err) {
-//     console.error("❌ Replicate Error:", err);
-//     res.status(500).json({ error: "Failed to generate" });
-//   }
-// });
+router.post("/test-scribble",testScribble);
 
 export default router;
